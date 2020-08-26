d7a4ad9a0191b553d3ec94e412fb3114b237a600
Eric LaFontsee <elafontsee@gmail.com>
2020-08-25 21:22:37 -0500

    Wrote README.md. Cleaned up javascript and fixed bugs. Added some addition styling.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Homework-CodeQuiz
+# Code Quiz
+##Description
+For our homework assignent, we were asked to create a timed code quiz. When the user hits start, they will have 60 seconds to answer 10 multiple choice questions. Every correctly answered question will earn the user a point. Every wrong answer will result in a penalty of 10 seconds removed from the count down timer.  
+If they successfully answer all 10 questions, they will be asked to enter their intials for the High Score List. Their score, along with their initials, will be displayed with a list of other high scores.  
+If the user does not succesfully answer all the questions before the timer runs out, they will not be asked to enter their intials.
+
+This app has been deployed to GitHub Pages. Try it out!
+https://ericlafontsee.github.io/Homework-CodeQuiz/
+
+##Usage
+![Code-Quiz Demo](images/Code-Quiz-Demo.gif)
+
+
+## Built With
+HTML  
+CSS  
+JavaScript 
+BootStrap 
+Background image from toptal.com
+
+## Prerequisites
+To build or edit this application you will need Visual Studio Code or text editor
+
+## Installation
+To utlize the background image from toptal.com, place the following url in your css as the background-Image value:
+
+www.toptal.com/designers/subtlepatterns/
+
+To utilize Bootstrap components, grid, and other styles, insert the following link into the head of your html document:
+
+```html
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+```
+
+## Author
+Eric LaFontsee 
+
+
+## License
+MIT License
+
+## Acknowledgments
+Anthony Cooper(Instructor) - For help with revisions and debugging of the javaScript.
--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var startBtn = document.querySelector("#start-button");
 var highScoreForm = document.querySelector("#highScoreForm");
 var timer = document.getElementById("timer");
 var currentQuestion = document.querySelector("#currentQuestion");
-var index = 0;
+var questionaireDiv = document.querySelector("#questionaire");
+var index = -1;
 var optionsUl = document.createElement("ul");
 optionsUl.style.listStyleType = "none";
 var modalEl = document.querySelector("#modal-container");
@@ -12,20 +13,18 @@ var closeBtn = document.querySelector(".close");
 var saveBtn = document.querySelector("#save");
 var highScoresList = document.querySelector("#highScoresList");
 var enterInitials = document.querySelector("#enterInitials");
-
+var highScorelink = document.querySelector("#highScoreLink");
+var gameOver = document.querySelector("#gameover");
+gameOver.style.display = "none";
+var currentTimer = 60;
 
 var highScores = [];
 var currentId = 0;
 var quiz = [
     {
-        question: "What data types can an array store?",
-        choices: ["strings", "booleans", "numbers", "All of the above"],
-        answer: "All of the above"
-    },
-    {
-        question: "Which of the following stores a value?",
-        choices: ["if statement", "boolean", "var", "event propagation"],
-        answer: "var"
+        question: "Which of the following can be assigned a value?",
+        choices: ["If statement", "Boolean", "Var", "Event propagation"],
+        answer: "Var"
     },
     {
         question: "How do you check that two variables are equal in value and in type?",
@@ -33,40 +32,66 @@ var quiz = [
         answer: "==="
     },
     {
-        question: "How do you print something to the console in JS?",
+        question: "How do you print something to the console in JavaScript?",
         choices: ["window.alert", "console.log()", "print.screen()", "print.console"],
         answer: "console.log()"
     },
     {
-        question: "You must convert celisus to farenhiet several times on your website. What is the best way to reuse the same equation in JS?",
-        choices: ["function", "variables", "for loop", "while loop"],
-        answer: "function"
+        question: "You must convert celisus to farenhiet several times on your website. What is the best way to reuse the same equation in JavaScript?",
+        choices: ["Function", "Variables", "A for loop", "A while loop"],
+        answer: "Function"
     },
     {
         question: "Which method finds the element with a class of 'test' in an HTML document?",
         choices: [".addEventListener('.test')", "document.querySelector('.test')", "document.getElementByID('#test')", "doc.find('.test')"],
         answer: "document.querySelector('.test')"
+    },
+    {
+        question: "If a variable is declared at the top of the JavaScript file and it is not nested it is a _____ variable?",
+        choices: ["Top-level", "Local", "Global", "King"],
+        answer: "Global"
+    },
+    {
+        question: "When you need to get files from a repository linked to your computer, you must perform what action in the terminal?",
+        choices: ["git add", "git commit", "git pull", "git clone"],
+        answer: "git pull"
+    },
+    {
+        question: "What is the shorthand for incrementing the variable 'i' by 1?",
+        choices: ["i = i + 1;", "i--;", "i++;", "i + 1;"],
+        answer: "i++;"
+    },
+    {
+        question: "Which of the following are JavaScript keywords",
+        choices: ["var", "let", "const", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "What data types can an array store?",
+        choices: ["Strings", "Booleans", "Numbers", "All of the above"],
+        answer: "All of the above"
     }
 ];
-var currentTimer = 60;
 
 function startTimer(event) {
     event.preventDefault()
     startBtn.style.display = "none";
+    modalEl.style.display = "none";
 
     var timeInterval = setInterval(function () {
         timer.textContent = "Time Remaining: " + currentTimer;
         currentTimer--;
 
         if (currentTimer === 0) {
-            timer.textContent = "TIMES UP!";
+            timer.textContent = "";
+            gameOver.style.display = "block";
+            highScorelink.style.display = "none";
             currentQuestion.textContent = "";
             optionsUl.style.display = "none";
-            saveScore(event);      //**** CHeck */
             clearInterval(timeInterval);
         } else if (index > quiz.length - 1) {
             timer.textContent = "";
-            saveScore();
+            init();
             clearInterval(timeInterval);
         }
 
@@ -121,14 +146,14 @@ function close() {
     currentTimer = 60;
 
 }
-function saveScore(event) {
-    modalEl.style.display = "block";
-    // saveBtn.addEventListener("click", function(event){
-    // modalEl.style.display = "none";
-
-    // })
-    init();
-}
+// function saveScore(event) {
+//     modalEl.style.display = "block";
+//     saveBtn.addEventListener("click", function(event){
+//     modalEl.style.display = "none";
+//     init();
+//     })
+   
+// }
 
 function renderHighScores() {
     highScoresList.innerHTML = "";
@@ -142,6 +167,8 @@ function renderHighScores() {
     }
 }
 function init() {
+    modalEl.style.display = "block";
+
     var storedHighScores = JSON.parse(localStorage.getItem("highScores"));
 
     if (storedHighScores) {
@@ -165,10 +192,21 @@ highScoreForm.addEventListener("submit", function (event) {
             initials: enterInitials.value,
             score: score
         };
+        enterInitials.value = "";
         highScores.push(userScore);
         storeHighScores();
         renderHighScores();
     }
+});
+
+highScorelink.addEventListener("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    init();
+    modalEl.style.display = "block";
+    // enterInitials.style.display = "none";
+    // saveBtn.style.display = "none";
+
 });
 
 closeBtn.addEventListener("click", close);
